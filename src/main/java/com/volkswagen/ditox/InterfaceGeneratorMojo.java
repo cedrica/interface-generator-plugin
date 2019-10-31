5e68f967755d22fac9ea9c5a500bd020896a8ff6
cedrica <djikeussi2001@yahoo.fr>
2019-10-31 11:30:05 +0100

    feature/VWDITOX-206  validate parameter and add required to fields

--- src/main/java/com/volkswagen/ditox/InterfaceGeneratorMojo.java
+++ src/main/java/com/volkswagen/ditox/InterfaceGeneratorMojo.java
@@ -1,5 +1,6 @@
 package com.volkswagen.ditox;
 
+import com.volkswagen.ditox.exception.EmptyParameterException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -10,22 +11,40 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.COMPILE)
 public class InterfaceGeneratorMojo extends AbstractMojo {
-	@Parameter private String outputDir;
-	@Parameter private String outputFileName;
-	@Parameter private String inputClass;
+	@Parameter(required = true) private String outputDir;
+	@Parameter(required = true) private String outputFileName;
+	@Parameter(required = true) private String inputClass;
 
 	@Override
-	public void execute() {
+	public void execute() throws EmptyParameterException {
+		if(inputClass == null || outputDir == null || outputFileName == null)
+			throw new EmptyParameterException("All parameter are required in oder for the plugin to work");
+
 		getLog().info("Interface convertion of the class: " + inputClass);
 		try {
 			Class clazz = Class.forName(inputClass);
 			String interfaceAsString = StringUtil.convertClassIntoInterfaceStr(clazz);
 			StringUtil.writeDataToOutput(interfaceAsString, outputDir, outputFileName);
 		} catch (ClassNotFoundException e) {
-			getLog().error("Error while instanciing "  + inputClass );
+			getLog().error("Error while instanciing "  + inputClass +": "+e.getMessage());
 		}
 		getLog().info("Convertion successfully done");
 	}
 
 
+	// generate setter for test
+	InterfaceGeneratorMojo setOutputDir(String outputDir) {
+		this.outputDir = outputDir;
+		return  this;
+	}
+
+	InterfaceGeneratorMojo setOutputFileName(String outputFileName) {
+		this.outputFileName = outputFileName;
+		return this;
+	}
+
+	InterfaceGeneratorMojo setInputClass(String inputClass) {
+		this.inputClass = inputClass;
+		return this;
+	}
 }
